1cfc6fd96d84a5c787eb0bbed4cc95e957569804
Krakaw <ks@tari.com>
2018-09-13 13:24:41 +0200

    Adding user endpoints

--- src/resources/users.ts
+++ src/resources/users.ts
@@ -1,48 +1,97 @@
-import { instanceOfRequestMethod } from "../interfaces/server/request-method.interface";
-import { ResourceInterface } from "../interfaces/server/resource";
+import {instanceOfRequestMethod} from "../interfaces/server/request-method.interface";
+import {ResourceInterface} from "../interfaces/server/resource";
 
 export default {
-  path: "users",
+    path: "users",
 
-  methods: [
-    instanceOfRequestMethod({
-      name: "create",
-      method: "POST",
-      path: "/register",
-      required: ["first_name", "last_name", "email", "phone", "password"],
-      requiresAuth: false
-    }),
+    methods: [
 
-    instanceOfRequestMethod({
-      name: "update",
-      method: "PUT",
-      path: "/me",
-      requireOne: ["first_name", "last_name", "email", "phone"],
-      requiresAuth: true
-    }),
+        /**
+         * Register a user
+         * @name create
+         * @return
+         */
+        instanceOfRequestMethod({
+            name: "create",
+            method: "POST",
+            path: "/register",
+            required: ["first_name", "last_name", "email", "phone", "password"],
+            requiresAuth: false
+        }),
 
-    instanceOfRequestMethod({
-      name: "current",
-      method: "GET",
-      path: "/me",
-      required: [],
-      requiresAuth: true
-    }),
+        /**
+         * Update your own user
+         */
+        instanceOfRequestMethod({
+            name: "update",
+            method: "PUT",
+            path: "/me",
+            requireOne: ["first_name", "last_name", "email", "phone"],
+            requiresAuth: true
+        }),
 
-    instanceOfRequestMethod({
-      name: "find",
-      method: "GET",
-      path: "",
-      required: ["email"],
-      requiresAuth: true
-    }),
+        /**
+         * Get your current user
+         */
+        instanceOfRequestMethod({
+            name: "current",
+            method: "GET",
+            path: "/me",
+            required: [],
+            requiresAuth: true
+        }),
 
-    instanceOfRequestMethod({
-      name: "show",
-      method: "GET",
-      path: "/{id}",
-      required: [],
-      requiresAuth: true
-    })
-  ]
+        /**
+         * List of users
+         * @name index
+         * @return Array<UserInterface>
+         */
+        instanceOfRequestMethod({
+            name: "index",
+            method: "GET",
+            path: "",
+            required: [],
+            requiresAuth: true
+        }),
+
+        /**
+         * Find a user
+         * @notimplemented
+         */
+        instanceOfRequestMethod({
+            name: "find",
+            method: "GET",
+            path: "",
+            required: ["email"],
+            requiresAuth: true
+        }),
+
+        /**
+         * Read a user
+         * @name read
+         * @param {id}
+         * @return UserInterface
+         */
+        instanceOfRequestMethod({
+            name: "read",
+            method: "GET",
+            path: "/{id}",
+            required: ['id'],
+            requiresAuth: true
+        }),
+
+        /**
+         * List of organizations for a user
+         * @name organization.index
+         * @return Array<UserInterface>
+         */
+        instanceOfRequestMethod({
+            namespace: 'organizations',
+            name: "index",
+            method: "GET",
+            path: "/{id}/organizations",
+            required: ['id'],
+            requiresAuth: true
+        }),
+    ]
 } as ResourceInterface;
