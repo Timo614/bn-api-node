a4a241ac6dba2ce0c29f7f9adcfc4cb94dc0fe95
Keith Simon <41575888+Krakaw@users.noreply.github.com>
2018-09-14 11:59:20 +0200

    Add js to eslint (#113)
    * Added validation and fixing to *.js files as well
    
    * Fixed eslint issues in js files

--- src/resources/index.js
+++ src/resources/index.js
@@ -1,28 +1,28 @@
-import Artist from './artists';
-import Auth from './auth';
-import Cart from './cart';
-import Event from './event';
-import External from './external';
-import Invitations from './invitations';
-import Organization from './organization';
-import Regions from './regions';
-import Status from './status';
-import TicketPricing from './ticket-pricing';
+import Artist from "./artists";
+import Auth from "./auth";
+import Cart from "./cart";
+import Event from "./event";
+import External from "./external";
+import Invitations from "./invitations";
+import Organization from "./organization";
+import Regions from "./regions";
+import Status from "./status";
+import TicketPricing from "./ticket-pricing";
 
-import Users from './users';
-import Venue from './venue';
+import Users from "./users";
+import Venue from "./venue";
 
 export default {
-    artists: Artist,
-    auth: Auth,
-    cart: Cart,
-    events: Event,
-    external: External,
-    organizations: Organization,
-    regions: Regions,
-    invitations: Invitations,
-    status: Status,
-    ticketPricing: TicketPricing,
-    users: Users,
-    venues: Venue
+	artists: Artist,
+	auth: Auth,
+	cart: Cart,
+	events: Event,
+	external: External,
+	organizations: Organization,
+	regions: Regions,
+	invitations: Invitations,
+	status: Status,
+	ticketPricing: TicketPricing,
+	users: Users,
+	venues: Venue
 }
