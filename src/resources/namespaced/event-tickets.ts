f40455f8142314d693a21399e8b42d1ea37f3eef
Jason van den Berg <jayvdb1@gmail.com>
2018-09-28 22:07:01 +0200

    Method for updating ticket details (#116)

--- src/resources/namespaced/event-tickets.ts
+++ src/resources/namespaced/event-tickets.ts
@@ -2,29 +2,43 @@ import { createRequestMethod } from "../../interfaces/server/request-method.inte
 import { ResourceInterface } from "../../interfaces/server/resource";
 
 export default {
-
 	methods: [
 		/**
-         * Create a ticket type
-         * @name create
-         * @param params {TicketInterface}
-         * @return TicketInterface
-         */
+		 * Create a ticket type
+		 * @name create
+		 * @param params {TicketInterface}
+		 * @return TicketInterface
+		 */
 		createRequestMethod({
 			namespace: "tickets",
 			name: "create",
 			method: "POST",
-			path: "/{id}/tickets",
-			required: ["id", "name"],
+			path: "/{event_id}/tickets",
+			required: ["event_id", "name"],
 			requiresAuth: true
 		}),
 
 		/**
-         * Get a list of tickets for the event
-         * @name index
-         * @param params {event_id}
-         * @return Array<TicketInterface>
-         */
+		 * Update a ticket type
+		 * @name update
+		 * @param params {TicketInterface}
+		 * @return TicketInterface
+		 */
+		createRequestMethod({
+			namespace: "tickets",
+			name: "update",
+			method: "PATCH",
+			path: "/{event_id}/tickets/{id}",
+			required: ["event_id", "id"],
+			requiresAuth: true
+		}),
+
+		/**
+		 * Get a list of tickets for the event
+		 * @name index
+		 * @param params {event_id}
+		 * @return Array<TicketInterface>
+		 */
 		createRequestMethod({
 			namespace: "tickets",
 			name: "index",
@@ -32,19 +46,8 @@ export default {
 			path: "/{id}/tickets",
 			required: ["id"],
 			requiresAuth: false
-		}),
+		})
 
-		// /**
-		//  * @notimplemented
-		//  */
-		// createRequestMethod({
-		//     name: "update",
-		//     method: "PUT",
-		//     path: "/{id}",
-		//     required: ["event_id", "id"],
-		//     requiresAuth: true
-		// }),
-		//
 		// /**
 		//  * @notimplemented
 		//  */
