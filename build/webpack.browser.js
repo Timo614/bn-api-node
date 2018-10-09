01bf6f8e1a837b9bbe2bca8468ffc470d43a480c
Keith Simon <41575888+Krakaw@users.noreply.github.com>
2018-10-09 09:14:23 +0200

    Compressed the web build (#138)
    Used eval as source map instead of inline

--- build/webpack.browser.js
+++ build/webpack.browser.js
@@ -1,30 +1,32 @@
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const webpack = require('webpack');
 
 const clientConfig = {
-    target: "web",
-    mode: "production",
-    devtool: "inline-source-map",
-    entry: "./src/index.ts",
-    output: {
-        filename: "bundle.client.js",
-        libraryTarget: 'umd',
-        library: 'Bigneon',
-        umdNamedDefine: true
-    },
-
-    resolve: {
-        // Add `.ts` and `.tsx` as a resolvable extension.
-        extensions: [".ts", ".tsx", ".js"]
-    },
-    plugins: [
-        // new BundleAnalyzerPlugin()
-    ],
-    module: {
-        rules: [
-            // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-            {test: /\.tsx?$/, loaders: ["babel-loader", "ts-loader"]}
-        ]
-    }
+	target: "web",
+	mode: "production",
+	devtool: "eval",
+	entry: "./src/index.ts",
+	output: {
+		filename: "bundle.client.js",
+		libraryTarget: "umd",
+		library: "Bigneon",
+		umdNamedDefine: true
+	},
+	resolve: {
+		// Add `.ts` and `.tsx` as a resolvable extension.
+		extensions: [".ts", ".tsx", ".js"]
+	},
+	plugins: [
+		new webpack.DefinePlugin({
+			'process.env.NODE_ENV': '"production"'
+		}),
+	],
+	module: {
+		rules: [
+			// all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+			{ test: /\.tsx?$/, loaders: ["babel-loader", "ts-loader"] }
+		]
+	}
 };
 
 module.exports = clientConfig;
--- build/webpack.server.js
+++ build/webpack.server.js
@@ -1,28 +1,28 @@
-const webpack = require('webpack');
+const webpack = require("webpack");
 const serverConfig = {
-  target: "node",
-  mode: "production",
-  devtool: "inline-source-map",
-  entry: "./src/index.ts",
-  output: {
-    filename: "bundle.node.js",
-    libraryTarget: 'umd',
-    library: 'Bigneon',
-    umdNamedDefine: true
-  },
-  resolve: {
-    // Add `.ts` and `.tsx` as a resolvable extension.
-    extensions: [".ts", ".tsx", ".js"]
-  },
-  plugins: [
-    new webpack.DefinePlugin({ "global.GENTLY": false })
-  ],
-  module: {
-    rules: [
-      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-      { test: /\.tsx?$/, loader: "ts-loader" }
-    ]
-  }
+	target: "node",
+	mode: "production",
+	devtool: "inline-source-map",
+	entry: "./src/index.ts",
+	output: {
+		filename: "bundle.node.js",
+		libraryTarget: "umd",
+		library: "Bigneon",
+		umdNamedDefine: true
+	},
+	resolve: {
+		// Add `.ts` and `.tsx` as a resolvable extension.
+		extensions: [".ts", ".tsx", ".js"]
+	},
+	plugins: [
+		new webpack.DefinePlugin({ "global.GENTLY": false })
+	],
+	module: {
+		rules: [
+			// all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+			{ test: /\.tsx?$/, loader: "ts-loader" }
+		]
+	}
 };
 
 module.exports = serverConfig;
