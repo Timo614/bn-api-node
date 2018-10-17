1df2b4ddb568a12371eead67db377f40e5f93e9c
Keith Simon <41575888+Krakaw@users.noreply.github.com>
2018-10-17 08:19:35 +0200

    Add cart id endpoint (#169)
    * Preparing for travis build
    
    * Added the /cart/{id} endpoint
    
    Added a new test to compare against the github repo of urls

--- test/04_endpoints.js
+++ test/04_endpoints.js
@@ -1,39 +1,40 @@
-const fs = require("fs");
+const axios = require("axios");
 const assert = require("assert");
 module.exports = ((global) => {
 	describe("Compare bn-api endpoints to bn-api-node endpoints", () => {
-		it("The bn-api endpoints should match bn-api-node endpoints", () => {
-			let routingPath = "../bn-api/api/src/routing.rs";
-			const routingExists = fs.existsSync(routingPath);
-			if (routingExists) {
-				const content= fs.readFileSync(routingPath, "utf8");
-				const regex = /"(\/.*?)"/g;
-				const apiUrls = content.match(regex).map(item => item.replace(/"/g, "").replace(/\{.*?\}/gi, "{id}"));
-				const localUrls = global.publicServer.matchingResources.map(item => `/${item.fullPath}`.replace(/\?.*$/, "")).filter((v, i, a) => a.indexOf(v) === i);
-				const removeIndexes = [];
-				apiUrls.forEach((url, index) => {
-					let pos = localUrls.indexOf(url);
-					if (pos > -1) {
-						removeIndexes.push(index);
-						localUrls.splice(pos, 1);
-					}
-				});
-				//Sort the indexes to remove descending
-				removeIndexes.sort((a,b) => {return a > b ? -1 : a < b ? 1 : 0});
-				removeIndexes.forEach(index => {
-					apiUrls.splice(index,1);
-				});
+		it("The bn-api endpoints should match bn-api-node endpoints", async () => {
+			let branch = process.env.TEST_ROUTING_BRANCH || "master";
+			let result = await axios.get(`https://raw.githubusercontent.com/big-neon/bn-api/${branch}/api/src/routing.rs`);
 
-				let message = "";
-				if (apiUrls.length) {
-					message += `\n${apiUrls.join(",")} missing from bn-api-node`;
+			const content = result.data;
+			const regex = /"(\/.*?)"/g;
+			const apiUrls = content.match(regex).map(item => item.replace(/"/g, "").replace(/\{.*?\}/gi, "{id}"));
+			const localUrls = global.publicServer.matchingResources.map(item => `/${item.fullPath}`.replace(/\?.*$/, "")).filter((v, i, a) => a.indexOf(v) === i);
+			const removeIndexes = [];
+			apiUrls.forEach((url, index) => {
+				let pos = localUrls.indexOf(url);
+				if (pos > -1) {
+					removeIndexes.push(index);
+					localUrls.splice(pos, 1);
 				}
-				if (localUrls.length) {
-					message += `\n${localUrls.join(",")} missing from routing.rs`;
-				}
+			});
+			//Sort the indexes to remove descending
+			removeIndexes.sort((a, b) => {
+				return a > b ? -1 : a < b ? 1 : 0
+			});
+			removeIndexes.forEach(index => {
+				apiUrls.splice(index, 1);
+			});
 
-				assert.strictEqual(apiUrls.length + localUrls.length, 0, message);
+			let message = "";
+			if (apiUrls.length) {
+				message += `\n${apiUrls.join(",")} missing from bn-api-node`;
 			}
+			if (localUrls.length) {
+				message += `\n${localUrls.join(",")} missing from routing.rs`;
+			}
+
+			assert.strictEqual(apiUrls.length + localUrls.length, 0, message);
 		});
 	})
 });
