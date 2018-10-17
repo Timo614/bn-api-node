24a1de12bd1972e4b4b5e315402832536d8931ee
Keith Simon <41575888+Krakaw@users.noreply.github.com>
2018-10-17 14:08:21 +0200

    142 add increment to ticket type (#174)
    * Add increment to ticket type
    
    * Generate docs

--- src/interfaces/resources/ticket-type.interface.ts
+++ src/interfaces/resources/ticket-type.interface.ts
@@ -9,6 +9,7 @@ export interface TicketTypeInterface {
     end_date: Date;
     quantity: number;
     limit: number;//Per person
+	increment: number;//How many can be bought at a time (4 per table)
     price_points: Array<TicketPricingInterface>;
     readonly created_at?: Date;
     readonly updated_at?: Date;
@@ -28,6 +29,7 @@ export const createTicketType = (base: any = {}): TicketTypeInterface => {
 			end_date: "",
 			quantity: 0,
 			limit: 0,//Per person
+			increment: 1,//In batches of 1
 			price_points: [],
 			created_at: "",
 			updated_at: "",
