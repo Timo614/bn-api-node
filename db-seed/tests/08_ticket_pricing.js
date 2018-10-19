ddcaebfa8ebe27fb0334a8b7c1d74729170ee49b
Keith Simon <41575888+Krakaw@users.noreply.github.com>
2018-10-19 14:30:13 +0200

    Krakaw 157 update ticket pricing (#182)
    * Adding ticket pricing tests
    
    * Added update ticket pricing test

---
+++ db-seed/tests/08_ticket_pricing.js
@@ -0,0 +1,37 @@
+const q = require("../queries");
+const assert = require("assert");
+const global = require("../helpers/globals");
+const ticketing = require("../helpers/ticketing");
+const interfaceFields = require("../../dist/interfaces/resources/event.interface");
+const assertFieldsMatch = require("../queries").assertFieldsMatch;
+
+describe("Integration::TicketPricing", () => {
+	let publicServer, adminServer;
+
+
+	describe("SuperUser Actions", () => {
+		let eventsByName = {};
+		before(async () => {
+			adminServer = await global.getAdminServer();
+			eventsByName = await global.getEvents();
+		});
+
+		it("SuperUser updates ticket pricing for event Fancy Pants Party", async () => {
+			let event = await adminServer.events.read({ id: eventsByName["Fancy Pants Party"] });
+			let ticketType = event.data.ticket_types.pop();
+
+			let updatedTicketTypes = await adminServer.events.ticketTypes.update({
+				event_id: event.data.id,
+				id: ticketType.id,
+				ticket_pricing: [{
+					name: "Updated Pricing",
+					status: "Published",
+					start_date: ticketType.start_date,
+					end_date: ticketType.end_date,
+					price_in_cents: 999999
+				}]
+			});
+			assert.strictEqual(updatedTicketTypes.status, 200);
+		});
+	})
+});
