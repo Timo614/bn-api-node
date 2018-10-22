3eee5560e90ba3e71473e823dd28aed743bd601d
Keith Simon <41575888+Krakaw@users.noreply.github.com>
2018-10-22 14:00:58 +0200

    Improve endpoint comparison (#184)
    Improve endpoint comparison

--- test/04_endpoints.js
+++ test/04_endpoints.js
@@ -1,5 +1,56 @@
 const axios = require("axios");
 const assert = require("assert");
+
+
+function processRoutingFile(fileContents) {
+	let lines = fileContents.split("\n");
+	let urls = {};//url: [METHOD]
+	let lastUrl;
+
+	const urlRegex = /"(\/.*?)"/g;
+	const methodRegex = /Method::([A-Z]+)/g;
+	for (let i in lines) {
+		let line = lines[i];
+		if (line.endsWith("register()")) {
+			break;
+		}
+		let urlMatch = line.match(urlRegex);
+		if (urlMatch && urlMatch.length) {
+			const apiUrls = urlMatch.map(item => item.replace(/"/g, "").replace(/\{.*?\}/gi, "{id}"));
+			lastUrl = apiUrls.pop();
+			if (!urls.hasOwnProperty(lastUrl)) {
+				urls[lastUrl] = {};
+			}
+		}
+
+		let methodMatches = line.match(methodRegex);
+		if (lastUrl && methodMatches && methodMatches.length) {
+			methodMatches = methodMatches.map(item => item.replace("Method::", "")).forEach(method => {
+				urls[lastUrl][method] = true;
+			});
+		}
+	}
+	return urls;
+}
+
+function compare(a, b) {
+	const missing = [];
+	for (let endpoint in a) {
+		if (!b.hasOwnProperty(endpoint)) {
+			//The entire url is missing
+			missing.push(Object.keys(a[endpoint]).join(", ") + " " + endpoint);
+		} else {
+			for (let method in a[endpoint]) {
+				if (!b[endpoint].hasOwnProperty(method)) {
+					missing.push(method + " " + endpoint);
+				}
+			}
+		}
+	}
+	return missing;
+}
+
+
 module.exports = ((global) => {
 	describe("Compare bn-api endpoints to bn-api-node endpoints", () => {
 		it("The bn-api endpoints should match bn-api-node endpoints", async () => {
@@ -7,34 +58,37 @@ module.exports = ((global) => {
 			let result = await axios.get(`https://raw.githubusercontent.com/big-neon/bn-api/${branch}/api/src/routing.rs`);
 
 			const content = result.data;
-			const regex = /"(\/.*?)"/g;
-			const apiUrls = content.match(regex).map(item => item.replace(/"/g, "").replace(/\{.*?\}/gi, "{id}"));
-			const localUrls = global.publicServer.matchingResources.map(item => `/${item.fullPath}`.replace(/\?.*$/, "")).filter((v, i, a) => a.indexOf(v) === i);
-			const removeIndexes = [];
-			apiUrls.forEach((url, index) => {
-				let pos = localUrls.indexOf(url);
-				if (pos > -1) {
-					removeIndexes.push(index);
-					localUrls.splice(pos, 1);
+			//Get the remote urls and their endpoints
+			const remoteUrlsAndMethods = processRoutingFile(content);
+
+			//Create a matching object of local endpoints and methods
+			const localUrlsAndMethods = {};
+			global.publicServer.matchingResources.map(item => {
+				item.fullPath = `/${item.fullPath}`.replace(/\?.*$/, "");
+				return item;
+			}).forEach(endpoint => {
+				if (!localUrlsAndMethods.hasOwnProperty(endpoint.fullPath)) {
+					localUrlsAndMethods[endpoint.fullPath] = {};
 				}
+				if (endpoint.clientOnly) {
+					return;
+				}
+				localUrlsAndMethods[endpoint.fullPath][endpoint.method] = true;
 			});
-			//Sort the indexes to remove descending
-			removeIndexes.sort((a, b) => {
-				return a > b ? -1 : a < b ? 1 : 0
-			});
-			removeIndexes.forEach(index => {
-				apiUrls.splice(index, 1);
-			});
+
+			const missingFromRemote = compare(localUrlsAndMethods, remoteUrlsAndMethods);
+			const missingFromLocal = compare(remoteUrlsAndMethods, localUrlsAndMethods);
+
 
 			let message = "";
-			if (apiUrls.length) {
-				message += `\n${apiUrls.join(",")} missing from bn-api-node`;
+			if (missingFromRemote.length) {
+				message += `\n${missingFromRemote.join(",")} missing from bn-api-node`;
 			}
-			if (localUrls.length) {
-				message += `\n${localUrls.join(",")} missing from routing.rs`;
+			if (missingFromLocal.length) {
+				message += `\n${missingFromLocal.join(",")} missing from routing.rs`;
 			}
 
-			assert.strictEqual(apiUrls.length + localUrls.length, 0, message);
+			assert.strictEqual(missingFromRemote.length + missingFromLocal.length, 0, message);
 		});
 	})
 });
