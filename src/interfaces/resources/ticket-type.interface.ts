618b2dbfa359541e95675b92b01a938ed170cd02
Keith Simon <41575888+Krakaw@users.noreply.github.com>
2018-11-12 11:35:54 +0200

    Limit per person (#218)
    * feat(field): limit_per_person added to ticketType
    
    * Generated docs

--- src/interfaces/resources/ticket-type.interface.ts
+++ src/interfaces/resources/ticket-type.interface.ts
@@ -8,7 +8,7 @@ export interface TicketTypeInterface {
     start_date: Date;
     end_date: Date;
     quantity: number;
-    limit: number;//Per person
+    limit_per_person: number;//Per person
 	increment: number;//How many can be bought at a time (4 per table)
     ticket_pricing: Array<TicketPricingInterface>;
     readonly created_at?: Date;
@@ -28,7 +28,7 @@ export const createTicketType = (base: any = {}): TicketTypeInterface => {
 			start_date: "",
 			end_date: "",
 			quantity: 0,
-			limit: 0,//Per person
+			limit_per_person: 0,//Per person
 			increment: 1,//In batches of 1
 			ticket_pricing: [],
 			created_at: "",
