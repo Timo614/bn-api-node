7b7d2aa6d81d9ec77887c8bcbcaed133ff0e875a
Keith Simon <41575888+Krakaw@users.noreply.github.com>
2018-11-16 12:24:28 +0200

    test(events): Use number of days for the start and door time (#227)

--- db-seed/tests/05_events.js
+++ db-seed/tests/05_events.js
@@ -4,19 +4,22 @@ const global = require("../helpers/globals");
 const ticketing = require("../helpers/ticketing");
 const interfaceFields = require("../../dist/interfaces/resources/event.interface");
 const assertFieldsMatch = require("../queries").assertFieldsMatch;
+const moment = require("moment");
 
-describe("Integration::Events", function() {
+describe("Integration::Events", function () {
 	let publicServer, adminServer;
 	const events = q.readCSV("./data/events.csv").map(event => {
 		event.age_limit = Number(event.age_limit);
+		event.event_start = isNaN(event.event_start) ? event.event_start : moment().add(event.event_start, "days").utc().format("YYYY-MM-DDTHH:mm:ss.SSS");
+		event.door_time = isNaN(event.door_time) ? event.door_time : moment().add(event.door_time, "days").utc().format("YYYY-MM-DDTHH:mm:ss.SSS");
 		return event;
 	});
 
-	describe("Public User", function() {
-		before(async function() {
+	describe("Public User", function () {
+		before(async function () {
 			publicServer = await global.getPublicServer();
 		});
-		it("an unauthenticated user cannot add an event", function() {
+		it("an unauthenticated user cannot add an event", function () {
 			return q.shouldFail(publicServer.events.create(events[0])).then(res => {
 				assert.strictEqual(
 					res.status,
@@ -27,10 +30,10 @@ describe("Integration::Events", function() {
 		});
 	});
 
-	describe("SuperUser Actions", function() {
+	describe("SuperUser Actions", function () {
 		let venuesByName = {}, orgsByName = {}, artistsByName = {};
 		global.eventsByName = {};
-		before(async function() {
+		before(async function () {
 			adminServer = await global.getAdminServer();
 			orgsByName = await global.getOrganizations();
 			venuesByName = await global.getVenues();
@@ -42,8 +45,7 @@ describe("Integration::Events", function() {
 			const event = events[i];
 			let id;
 			it(`SuperUser creates event "${event.name}" for Venue: ${
-				event.venue_id
-			} and Org: ${event.organization_id}`, async function() {
+				event.venue_id} and Org: ${event.organization_id}`, async function () {
 
 				event.venue_id = venuesByName[event.venue_id];
 				event.organization_id = orgsByName[event.organization_id];
@@ -59,23 +61,28 @@ describe("Integration::Events", function() {
 
 			it(`SuperUser adds Daft Punk artists to event ${event.name}`, async () => {
 				let artistId = artistsByName["Daft Punk"];
-				let result = await adminServer.events.artists.create({event_id: id, artist_id: artistId, rank: 0, set_time: "2018-12-12T12:00:00.000"});
+				let result = await adminServer.events.artists.create({
+					event_id: id,
+					artist_id: artistId,
+					rank: 0,
+					set_time: "2018-12-12T12:00:00.000"
+				});
 				assert.strictEqual(result.status, 201);
 				assert.strictEqual(result.data.artist_id, artistId, "Artist ID's must match");
 			});
 
 			if (i == 0) {
 				//Only add Billy Joel to the first event
-				it(`SuperUser Adds Billy Joel to event ${event.name}`, async() => {
+				it(`SuperUser Adds Billy Joel to event ${event.name}`, async () => {
 					let dpArtistId = artistsByName["Daft Punk"];
 					let bjArtistId = artistsByName["Billy Joel"];
 
 					let artists = [
-						{artist_id: dpArtistId, rank: 0, set_time: "2018-12-12T12:00:00.000"},
-						{artist_id: bjArtistId, rank: 1, set_time: "2018-12-12T14:00:00.000"},
+						{ artist_id: dpArtistId, rank: 0, set_time: "2018-12-12T12:00:00.000" },
+						{ artist_id: bjArtistId, rank: 1, set_time: "2018-12-12T14:00:00.000" },
 					];
 
-					let result = await adminServer.events.artists.update({event_id: id, rank: 0, artists});
+					let result = await adminServer.events.artists.update({ event_id: id, rank: 0, artists });
 					assert.strictEqual(result.status, 200);
 					assert.strictEqual(result.data.length, 2, "We are expecting 2 artists back");
 				});
@@ -83,8 +90,7 @@ describe("Integration::Events", function() {
 
 
 			it(`SuperUser adds ticket pricing to event "${
-				event.name
-			}"`, async function() {
+				event.name}"`, async function () {
 				const testTicketTypes = [
 					{
 						name: "General Access",
@@ -132,20 +138,18 @@ describe("Integration::Events", function() {
 					assert.strictEqual(
 						result.status,
 						201,
-						`${name} ticket type was not added to event ${
-							event.name
-						}; ${ticketTypeDetails}`
+						`${name} ticket type was not added to event ${event.name}; ${ticketTypeDetails}`
 					);
 				}
 			});
 
-			it(`SuperUser publishes event "${event.name}"`, async function() {
+			it(`SuperUser publishes event "${event.name}"`, async function () {
 				const result = await adminServer.events.publish({ id });
 				assert.strictEqual(result.status, 200);
 			});
 		}
 
-		it("A superuser can update an event", async function() {
+		it("A superuser can update an event", async function () {
 
 			let updatedEvent = {
 				id: global.eventsByName["Update This Event"],
@@ -156,7 +160,7 @@ describe("Integration::Events", function() {
 				promo_image_url: "https://res.cloudinary.com/bigneon-dev/image/upload/v1540996739/bigneon/pssets0z4vxrjasvhvmj.jpg"
 			};
 			const response = await adminServer.events.update(updatedEvent);
-			const serverEvent = (await adminServer.events.read({id: global.eventsByName["Update This Event"]})).data;
+			const serverEvent = (await adminServer.events.read({ id: global.eventsByName["Update This Event"] })).data;
 			assert.strictEqual(response.status, 200);
 
 			for (let i in updatedEvent) {
@@ -169,27 +173,27 @@ describe("Integration::Events", function() {
 
 	});
 
-	describe("Event lists", function() {
+	describe("Event lists", function () {
 		let event;
-		before(async function() {
+		before(async function () {
 			publicServer = await global.getPublicServer();
 		});
 		let list;
-		it("an unauthenticated user can retrieve the published event list", async function() {
+		it("an unauthenticated user can retrieve the published event list", async function () {
 			const response = await publicServer.events.index();
 			assert.strictEqual(response.status, 200);
 			list = response.data.data;
 			assert.strictEqual(list.length, events.length);
 		});
 
-		it("an unauthenticated user can retrieve an event", async function() {
+		it("an unauthenticated user can retrieve an event", async function () {
 			const response = await publicServer.events.read({ id: list[0].id });
 			event = response.data;
 			assert.strictEqual(response.status, 200);
 			assert.strictEqual(response.data.name, list[0].name);
 		});
 
-		it(`an unauthenticated user can search for an event by name: ${events[0].name}`, async function() {
+		it(`an unauthenticated user can search for an event by name: ${events[0].name}`, async function () {
 			const response = await publicServer.events.index({ query: events[0].name });
 			assert.strictEqual(response.status, 200, "Invalid Status");
 			const matches = response.data.data;
@@ -197,7 +201,7 @@ describe("Integration::Events", function() {
 			assert.strictEqual(matches[0].name, events[0].name);
 		});
 
-		it(`an unauthenticated user can search for an event by Artist: Billy Joel}`, async function() {
+		it(`an unauthenticated user can search for an event by Artist: Billy Joel}`, async function () {
 			const response = await publicServer.events.index({ query: "Billy Joel" });
 			assert.strictEqual(response.status, 200, "Invalid Status");
 			const matches = response.data.data;
@@ -205,7 +209,7 @@ describe("Integration::Events", function() {
 			assert.strictEqual(matches[0].name, "Fancy Pants Party");
 		});
 
-		it(`an unauthenticated user can search for an event by Venue: Loftus Versveld Clubhouse}`, async function() {
+		it(`an unauthenticated user can search for an event by Venue: Loftus Versveld Clubhouse}`, async function () {
 			const response = await publicServer.events.index({ query: "Loftus Versveld Clubhouse" });
 			assert.strictEqual(response.status, 200, "Invalid Status");
 			const matches = response.data.data;
@@ -214,7 +218,7 @@ describe("Integration::Events", function() {
 		});
 
 
-		it("an unauthenticated user cannot update an event", function() {
+		it("an unauthenticated user cannot update an event", function () {
 			return q
 				.shouldFail(
 					publicServer.events.update({ id: list[0].id, name: "My First Event" })
@@ -229,8 +233,7 @@ describe("Integration::Events", function() {
 		});
 
 
-
-		it("Compares Interface with Response", async() => {
+		it("Compares Interface with Response", async () => {
 			let result = await adminServer.events.read({ id: list[0].id });
 			let event = result.data;
 			let localInterface = interfaceFields.createEvent();
