2455ce94e60e68a5ddbb6a4f5bfc8c1809d8067f
Keith Simon <41575888+Krakaw@users.noreply.github.com>
2018-12-13 13:05:03 +0200

    Fixed org roles (#260)
    * Added org user roles and descriptions
    
    * Build docs

--- src/interfaces/enums/index.ts
+++ src/interfaces/enums/index.ts
@@ -1,10 +1,11 @@
 import { OverrideStatus as EventOverrideStatus, OVERRIDE_STATUS_STRING as EVENT_OVERRIDE_STATUS_STRING } from "./events.enum";
-import { UserRole } from "./user-roles.enum";
+import { UserRole, USER_ROLES_STRING } from "./user-roles.enum";
 import { DeviceTokenSource } from "./device-tokens.enum";
 
 export default {
 	EventOverrideStatus,
 	EVENT_OVERRIDE_STATUS_STRING,
 	UserRole,
+	USER_ROLES_STRING,
 	DeviceTokenSource,
 }
--- src/interfaces/enums/user-roles.enum.ts
+++ src/interfaces/enums/user-roles.enum.ts
@@ -1,3 +1,15 @@
 export enum UserRole {
-	Admin, OrgMember, OrgOwner, OrgAdmin, OrgBoxOffice, DoorPerson, User
+	ORG_MEMBER = "OrgMember",
+	ORG_OWNER = "OrgOwner",
+	ORG_ADMIN = "OrgAdmin",
+	ORG_BOX_OFFICE = "OrgBoxOffice",
+	DOOR_PERSON = "DoorPerson",
 }
+
+export const USER_ROLES_STRING = {
+	[UserRole.ORG_OWNER]: "Owner",
+	[UserRole.ORG_ADMIN]: "Admin",
+	[UserRole.ORG_MEMBER]: "Member",
+	[UserRole.ORG_BOX_OFFICE]: "Box Office",
+	[UserRole.DOOR_PERSON]: "Door Person",
+};
