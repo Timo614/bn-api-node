df8c8f9e80aadf9a1dd10a3447416b1a0eba2cb7
Mike the Tike <stringhandler@gmail.com>
2019-02-19 14:08:17 +0200

    Add sold out behavior (#312)

--- src/interfaces/resources/ticket-type.interface.ts
+++ src/interfaces/resources/ticket-type.interface.ts
@@ -1,25 +1,31 @@
-import { createTicketPricing, TicketPricingInterface } from "./ticket-pricing.interface";
+import {
+	createTicketPricing,
+	TicketPricingInterface
+} from "./ticket-pricing.interface";
 
 export interface TicketTypeInterface {
-    readonly id?: string;
-    event_id: string;
-    name: string;
-    description?: string;
-    start_date: Date;
-    end_date: Date;
-    quantity: number;
-    limit_per_person: number;//Per person
-	increment: number;//How many can be bought at a time (4 per table)
-	price_in_cents: number,
-    ticket_pricing: Array<TicketPricingInterface>;
-    readonly created_at?: Date;
-    readonly updated_at?: Date;
-    readonly cancelled_at?: Date;
+	readonly id?: string;
+	event_id: string;
+	name: string;
+	description?: string;
+	start_date: Date;
+	end_date: Date;
+	quantity: number;
+	limit_per_person: number; //Per person
+	increment: number; //How many can be bought at a time (4 per table)
+	price_in_cents: number;
+	ticket_pricing: Array<TicketPricingInterface>;
+	sold_out_behavior: string;
+	readonly created_at?: Date;
+	readonly updated_at?: Date;
+	readonly cancelled_at?: Date;
 }
 
 export const createTicketType = (base: any = {}): TicketTypeInterface => {
 	if (base && base.ticket_pricing) {
-		base.ticket_pricing = base.ticket_pricing.map((ticketPricing: any) => createTicketPricing(ticketPricing));
+		base.ticket_pricing = base.ticket_pricing.map((ticketPricing: any) =>
+			createTicketPricing(ticketPricing)
+		);
 	}
 	return {
 		...{
@@ -30,13 +36,14 @@ export const createTicketType = (base: any = {}): TicketTypeInterface => {
 			start_date: "",
 			end_date: "",
 			quantity: 0,
-			limit_per_person: 0,//Per person
-			increment: 1,//In batches of 1
+			limit_per_person: 0, //Per person
+			increment: 1, //In batches of 1
 			price_in_cents: 0,
 			ticket_pricing: [],
+			sold_out_behavior: "",
 			created_at: "",
 			updated_at: "",
-			cancelled_at: null,
+			cancelled_at: null
 		},
 		...base
 	} as TicketTypeInterface;
